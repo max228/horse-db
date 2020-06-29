c9753f96b77c783e8465f9a638e8d9bc3f28cacf
max_ive <zhanbolot_m@auca.kg>
2020-06-29 12:35:57 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# horseDB project
+
+Simple app for finding authentic species of Kyrgyz horse
+
+## How to run
+
+Development environment:
+
+`yarn run dev`
+
+Production
+
+`yarn run build && yarn start`
---
+++ schema/schema.js
@@ -0,0 +1,96 @@
+const { Text, CalendarDay, Select, Relationship, Integer } = require('@keystonejs/fields')
+
+const ownerType = [
+  'Чабан',
+  'Коневодство',
+  'Владелец фермы',
+  'КРС'
+]
+
+const Owner = {
+  fields: {
+    name: {
+      type: Text,
+      isRequired: true,
+      label: 'ФИО'
+    },
+    birth: {
+      type: CalendarDay,
+      yearRangeFrom: '1990',
+      yearRangeTo: '2020',
+      isRequired: true,
+      label: 'Дата Рождения'
+    },
+    field: {
+      type: Select,
+      options: ownerType,
+      dataType: 'string',
+      label: 'Род деятельности'
+    },
+    address: {
+      type: Text,
+      label: 'Адрес'
+    },
+    phone: {
+      type: Text,
+      label: 'Номер телефона'
+    },
+    horses: {
+      type: Relationship,
+      ref: 'Horse.owner',
+      many: true,
+      label: 'Лошади'
+    }
+  }
+}
+
+const Horse = {
+  fields: {
+    name: {
+      type: Text,
+      isRequired: true
+    },
+    birth: {
+      type: CalendarDay,
+      yearRangeFrom: '1990',
+      yearRangeTo: '2020',
+      isRequired: true,
+      label: 'Дата Рождения'
+    },
+    owner: {
+      type: Relationship,
+      ref: 'Owner.horses',
+      many: false,
+      label: 'Хозяин'
+    },
+    height: {
+      type: Integer,
+      label: 'Высота в холке'
+    },
+    width: {
+      type: Integer,
+      label: 'Обхват, ширина груди'
+    },
+    long: {
+      type: Integer,
+      label: 'Косая длина туловища'
+    },
+    mouth: {
+      type: Integer,
+      label: 'Обхват пясти',
+    },
+    head: {
+      type: Integer,
+      label: 'Обхват головы'
+    },
+    neck: {
+      type: Integer,
+      label: 'Обхват шеи'
+    }
+  }
+}
+
+module.exports = {
+  Owner,
+  Horse
+}
